2f4c0587aa4ba906840280523db9aaaeaecefbcc
yaacovk10 <yaacovana@gmail.com>
2023-11-20 15:55:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+    const myStyle = {
+        backgroundColor : 'blue',
+      };
+    const [n1, setn1] = useState(0)
+    const [n2, setn2] = useState(0)
+    const [result, setresult] = useState(0)
+
+    const calc = (opr)=>{
+        setresult(eval(`${n1} ${opr} ${n2}`))
+    }
+    const changestyle = ()=>{
+       newstyle =  n1 *n2 * 100
+    }
+    return (
+    <div>
+        <input onChange={(e)=>setn1(e.target.value)}/>
+        <input onChange={(e)=>setn2(e.target.value)}/>
+
+        <button onClick= {(e)=>calc(e.target.innerText)} style={myStyle}>+</button>
+        <button onClick= {(e)=>calc(e.target.innerText)} >-</button>
+        <button onClick= {(e)=>calc(e.target.innerText)}>*</button>
+        <button onClick= {(e)=>calc(e.target.innerText)}>/</button>
+
+
+
+
+        <h2>Result:{result}</h2>
     </div>
-  );
-}
+    )
+    }
 
-export default App;
+export default App
